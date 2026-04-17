b1398467e95f2bc7eeccd13dddb14cf77b1a6136
Edith Vidal <ecsr.vidal@gmail.com>
2026-04-17 12:31:45 -0300

    Adicionando título da página

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Dashboard de KPIs",
+    page_title="Dashboard de KPIs de Usuários de E-commerce",
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
+st.title("Dashboard de KPIs de Usuários de E-commerce")
