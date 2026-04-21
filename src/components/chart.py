2ac0d8e7eb7a88837508cb7e515425f1c44cc956
Edith Vidal <ecsr.vidal@gmail.com>
2026-04-21 18:23:52 -0300

    Corrigindo chart para funcionar com pie_chart

--- src/components/chart.py
+++ src/components/chart.py
@@ -2,25 +2,11 @@ import streamlit as st
 import polars as pl
 from plotly.graph_objects import Figure
 
-_LAYOUT_MARGIN = dict[str, int](
-    l=52,
-    r=28,
-    t=72,
-    b=0,
-)
-_AXIS_TICK_FONT = dict[str, int](
-    size=14,
-)
-_XAXIS = dict[str, str | dict[str, int] | int](
-    title="",
-    tickfont=_AXIS_TICK_FONT,
-    tickangle=0,
-)
-_YAXIS = dict[str, str | dict[str, int]](
-    title="",
-    tickfont=_AXIS_TICK_FONT,
-)
-_LEGEND = dict[str, str | float](
+_LAYOUT_MARGIN = dict(l=52, r=28, t=72, b=0)
+_AXIS_TICK_FONT = dict(size=14)
+_XAXIS = dict(title="", tickfont=_AXIS_TICK_FONT, tickangle=0)
+_YAXIS = dict(title="", tickfont=_AXIS_TICK_FONT)
+_LEGEND = dict(
     title_text="",
     orientation="h",
     yanchor="bottom",
@@ -45,8 +31,6 @@ class Chart:
         self.df = df
 
     def plot(self, fig: Figure) -> None:
-        is_pie = bool(fig.data and getattr(fig.data[0], "type", None) == "pie")
-
         fig.update_layout(
             dragmode=False,
             title=self.title,
@@ -58,25 +42,23 @@ class Chart:
             legend=_LEGEND,
             showlegend=True,
         )
-        if not is_pie:
-            fig.update_traces(
-                hovertemplate="%{x}: %{y}<extra></extra>",
-                texttemplate="%{y}",
-                textposition="auto",
-                textfont_size=14,
-                textfont_color="white",
-                textfont_weight="bold",
-            )
-            max_total = self.df.group_by(self.x).agg(pl.col(self.y).sum()).max()[self.y]
-            fig.update_yaxes(range=[0, max_total * 1.1])
-            fig.update_xaxes(range=[0, self.df[self.x].max()])
-        else:
+        is_pie = bool(fig.data and getattr(fig.data[0], "type", None) == "pie")
+        if is_pie:
             fig.update_traces(
                 textinfo="percent",
                 hovertemplate="%{label}: %{value} (%{percent})<extra></extra>",
-                textposition="auto",
-                textfont_size=14,
-                textfont_color="white",
-                textfont_weight="bold",
             )
+            return
+
+        max_total = self.df.group_by(self.x).agg(pl.col(self.y).sum()).max()[self.y]
+        fig.update_yaxes(range=[0, max_total * 1.1])
+        fig.update_xaxes(range=[0, self.df[self.x].max()])
+        fig.update_traces(
+            hovertemplate="%{x}: %{y}<extra></extra>",
+            texttemplate="%{y}",
+            textposition="auto",
+            textfont_size=14,
+            textfont_color="white",
+            textfont_weight="bold",
+        )
         st.plotly_chart(fig, use_container_width=True)
